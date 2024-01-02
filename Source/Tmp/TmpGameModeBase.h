1c85551033daca312edf82549ae4bfd3551a86de
Lasoj <88119690+allelaok@users.noreply.github.com>
2024-01-02 11:33:10 +0900

    Create Unreal Project

---
+++ Source/Tmp/TmpGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TmpGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TMP_API ATmpGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
